4d0dec072531f0803e4aaec9a920ae3d2d87c304
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2024-10-17 09:24:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# live-odds-lib
+
+A Live Football World Cup Scoreboard library
---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "com.sportradar"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
